8e5f85ffaa5f6120cc5d3a472358245eddc6cfcd
Krzysiek S <krzszk96@gmail.com>
2022-07-19 10:17:17 +0200

    app init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
+// Firestore modules ______________________________________
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAKO8LuJ1BHNKXyl7SjJa_a9uX1o6MoEEs",
+  authDomain: "angulargroceries.firebaseapp.com",
+  projectId: "angulargroceries",
+  storageBucket: "angulargroceries.appspot.com",
+  messagingSenderId: "1074478989562",
+  appId: "1:1074478989562:web:7b3adf0abe204d37b54943"
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +25,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    // Firestore modules ______________________________________
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
