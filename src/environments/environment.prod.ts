cb010c7b2d14cd78e2d59ab540e43e7f01047b00
Krzysiek S <krzszk96@gmail.com>
2022-07-25 12:31:21 +0200

    test deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAKO8LuJ1BHNKXyl7SjJa_a9uX1o6MoEEs",
+    authDomain: "angulargroceries.firebaseapp.com",
+    databaseURL: "https://angulargroceries-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "angulargroceries",
+    storageBucket: "angulargroceries.appspot.com",
+    messagingSenderId: "1074478989562",
+    appId: "1:1074478989562:web:7b3adf0abe204d37b54943"
+  }
 };
